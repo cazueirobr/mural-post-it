a29c350f16a15860be253a01045862313ced7946
cazueirobr <matheus.sydor@hotmail.com>
2023-04-08 15:45:36 -0300

    feat: projeto estavel

--- src/main/java/br/com/edu/utfpr/muralpostit/Controller/HomeController.java
+++ src/main/java/br/com/edu/utfpr/muralpostit/Controller/HomeController.java
@@ -1,20 +1,57 @@
 package br.com.edu.utfpr.muralpostit.Controller;
 
+import br.com.edu.utfpr.muralpostit.util.Constants;
 import jakarta.servlet.*;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
 
 import java.io.IOException;
 
-@WebServlet(name = "HomeServlet", value = "/Home")
+@WebServlet(name = "HomeServlet", value = "/home")
 public class HomeController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        request.getRequestDispatcher("index.jsp").forward(request, response);
+        counterOfVisitors(request, response);
+        request.getRequestDispatcher("/WEB-INF/view/screen/index.jsp").forward(request, response);
     }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    public void counterOfVisitors(HttpServletRequest request, HttpServletResponse response){
+        boolean meuAtributo = (boolean) request.getAttribute("visitor");
+        if (meuAtributo == true) {
+
+            Integer notasCriadas = (Integer) request.getServletContext().getAttribute(Constants.COUNTER_APPLICATION);
+            if (notasCriadas == null) {
+                notasCriadas = 0;
+            }
+            notasCriadas++;
+
+            getServletContext().setAttribute(Constants.COUNTER_APPLICATION, notasCriadas);
+            Cookie visitCookie = new Cookie("visitante", String.valueOf(1));
+
+
+            visitCookie.setMaxAge(60);
+            response.addCookie(visitCookie);
+
+
+            int sessionNumberVisit = notasCriadas;
+            System.out.println(sessionNumberVisit);
+
+            request.getSession(true).setAttribute(Constants.COUNTER_SESSION, sessionNumberVisit);
+
+
+            Cookie numberVisit = new Cookie("numero_visitante", String.valueOf(notasCriadas));
+            response.addCookie(numberVisit);
+        }
+        else {
+
+
+        }
+
 
     }
 }
--- src/main/java/br/com/edu/utfpr/muralpostit/Controller/MainController.java
+++ src/main/java/br/com/edu/utfpr/muralpostit/Controller/MainController.java
@@ -6,15 +6,31 @@ import jakarta.servlet.annotation.*;
 
 import java.io.IOException;
 
-@WebServlet(name = "MainServlet", value = "/")
+@WebServlet(name = "MainServlet", value = "")
 public class MainController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        request.getRequestDispatcher("/Home").forward(request, response);
+        boolean visitor = isVisitor(request);
+        request.setAttribute("visitor",visitor);
+        request.getRequestDispatcher("/home").forward(request, response);
     }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
+    }
+
+    public boolean isVisitor(HttpServletRequest request){
+
+        Cookie[] cookies = request.getCookies();
+        String cookieValue = null;
+        if (cookies != null) {
+            for (Cookie cookie : cookies) {
+                if (cookie.getName().equals("visitante")) {
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 }
